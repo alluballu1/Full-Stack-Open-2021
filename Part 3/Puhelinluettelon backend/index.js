0498403a2037396a80a2aabee9f89f6b83d07547
alluballu <axu.porri@gmail.com>
2021-12-23 16:08:25 +0200

    first commit

--- Part 3/Puhelinluettelon backend/index.js
+++ Part 3/Puhelinluettelon backend/index.js
@@ -118,6 +118,7 @@ app.post("/api/persons", (request, response) => {
   response.end(JSON.stringify(phonebook));
 });
  */
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
